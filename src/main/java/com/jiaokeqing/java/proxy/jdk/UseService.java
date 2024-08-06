ad5ce9192e3461c68e2c2758a948e2993d76fcff
jiaokeqing <13583243451@163.com>
2024-08-06 11:14:04 +0800

    feat: Implement JDK Dynamic Proxy
    Added the DebugInvocationHandler class to provide dynamic proxy functionality by implementing the InvocationHandler interface.
    Introduced the JdkProxyFactory class with a static getProxy method to create proxy instances.
    Enhanced the documentation for UseService and UseServiceImpl classes with additional examples.

---
+++ src/main/java/com/jiaokeqing/java/proxy/jdk/UseService.java
@@ -0,0 +1,18 @@
+package com.jiaokeqing.java.proxy.jdk;
+
+/**
+ * The {@code UseService} class represents a service for usage operations and includes a {@code use} operation.
+ * <p>
+ * This class is designed to demonstrate how to perform usage-related tasks.
+ * </p>
+ *
+ * @author jiaokeqing
+ * @version 1.0
+ * @since 2024-08-06
+ */
+public interface UseService {
+    /**
+     * usage operations
+     */
+    void use();
+}
